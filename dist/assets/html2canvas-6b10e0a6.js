d49e58cb1dfc1bfa9b6668e6c530c27b14095c28
fxsmileing <2841005765@qq.com>
2025-05-31 21:52:03 +0800

    0002
    2cip

---
+++ dist/assets/html2canvas-6b10e0a6.js
@@ -0,0 +1,2 @@
+import{g as e,r as t}from"./html2pdf-6a6fa956.js";function r(e,t){for(var r=0;r<t.length;r++){const o=t[r];if("string"!=typeof o&&!Array.isArray(o))for(const t in o)if("default"!==t&&!(t in e)){const r=Object.getOwnPropertyDescriptor(o,t);r&&Object.defineProperty(e,t,r.get?r:{enumerable:!0,get:()=>o[t]})}}return Object.freeze(Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}))}var o=t();const n=r({__proto__:null,default:e(o)},[o]);export{n as h};
+//# sourceMappingURL=html2canvas-6b10e0a6.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,28 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    target: 'esnext',
+    sourcemap: true,
+    chunkSizeWarningLimit: 800,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'pinia'],
+          'html2pdf': ['html2pdf.js']
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
